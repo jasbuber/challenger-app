13dd72aab3f1a291ddf84ecb825e2dcced724322
Tomasz Jasianek <jasbuber@gmail.com>
2014-04-04 22:00:24 +0200

    A little change so it will compile

--- app/services/ChallengeService.java
+++ app/services/ChallengeService.java
@@ -2,18 +2,21 @@ package services;
 
 import domain.Challenge;
 import domain.ChallengeParticipation;
+import domain.User;
 import repositories.ChallengesRepository;
 
 public class ChallengeService {
 
     private final ChallengesRepository challengesRepository;
+    private final User user;
 
-    public ChallengeService(ChallengesRepository challengesRepository) {
+    public ChallengeService(ChallengesRepository challengesRepository, User user) {
         this.challengesRepository = challengesRepository;
+        this.user = user;
     }
 
     public Challenge createChallenge() {
-        return challengesRepository.createChallenge();
+        return challengesRepository.createChallenge(user);
     }
 
     public ChallengeParticipation participateInChallenge(Challenge challenge) {

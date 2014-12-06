ca22f4536dcd7a1a6b8c5f7a23519f65fe427ffa
bkaflowski <bartlomiej.kaflowski@gmail.com>
2014-12-06 18:07:36 +0100

    missing video file exception

---
+++ app/services/UploadVideoFileException.java
@@ -0,0 +1,10 @@
+package services;
+
+import java.io.IOException;
+
+public class UploadVideoFileException extends RuntimeException {
+
+    public UploadVideoFileException(Throwable cause) {
+        super(cause);
+    }
+}
